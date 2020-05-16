e60f70ade419076285789f56fbdc68f823b8e476
mrboggii <pruteanbogdan@gmail.com>
2020-05-16 17:24:51 +0200

    kata 1 resuelta

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -1,23 +1,56 @@
-from random import randint
+from random import* 
 
-options = ["Piedra", "Papel", "Tijeras"]
+# El usuario escoje
+def opcionJugador():
+ print("\t\t\tEscoje una opción\n")
+ print("\t\t1). Piedra. 2). Papel. 3). Tijera.", end=' ')
+ opcion = int(input("---> "))
+ if opcion == 1:
+  return "PIEDRA"
+ elif opcion == 2:
+  return "PAPEL"
+ else :
+  return "TIJERA"
 
-# El resultado de salida son las siguientes String
-#'Empate!'
-#'Ganaste!'
-#'Perdiste!'
-def quienGana(player, ai):
-    return ""
+# La maquina escoje
+def opcionMaquina():
+ opcion = randint(1, 3)
+ if opcion == 1:
+  return "PIEDRA"
+ elif opcion == 2:
+  return "PAPEL"
+ else :
+  return "TIJERA"
+# Se define el ganador
+def ganador(player, machine):
+ if player == machine:
+  return "EMPATE"
+ # -----------------------------
+ if player == "PIEDRA" and machine == "PAPEL":
+  return "La maquina GANA!"
+ if player == "PIEDRA" and machine == "TIJERA":
+  return "!GANASTE"
+ # -----------------------------
+ if player == "PAPEL" and machine == "PIEDRA":
+  return "GANASTE!"
+ if player == "PAPEL" and machine == "TIJERA":
+  return "La maquina gana!"
+ # -----------------------------
+ if player == "TIJERA" and machine == "PIEDRA":
+  return "La maquina gana!"
+ if player == "TIJERA" and machine == "PAPEL":
+  return "GANASTE!"
+# Muestra el resultado
+def resultado():
+ j = opcionJugador()
+ m = opcionMaquina()
+ g = ganador(j, m)
+ print("--------------------------------------")
+ print("      |JUGADOR| VS |MAQUINA|")
+ print("--------------------------------------")
+ print("\t  ", j, "\t   ", m)
+ print("\n\t\t   ", g)
+ print("--------------------------------------")
 
-# Entry Point
-def Game():
-    #
-    #
-    
-    #
-    #
-    
-    winner = quienGana(player, ai)
 
-    print(winner)
-
+resultado()
